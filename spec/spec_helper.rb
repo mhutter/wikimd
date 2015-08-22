c841a60e4fce38466e126cfbd93cad511fc52b6b
Manuel Hutter <manuel@hutter.io>
2015-08-22 16:55:11 +0200

    disabled profiling

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,6 @@
 ENV['RACK_ENV'] = 'test'
 require 'simplecov'
 SimpleCov.start
-# See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-# See .rspec
 
 # Fixture repo is included in the GIT repository
 FIXTURE_REPO_PATH = File.expand_path('../fixtures/repo', __FILE__)
@@ -11,6 +9,8 @@ TMP_REPO_PATH = File.expand_path('../../tmp/test_repo', __FILE__)
 # remember the original PWD
 ORIGINAL_PWD = Dir.pwd
 
+# See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+# See .rspec
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4.
@@ -38,7 +38,9 @@ RSpec.configure do |config|
     config.default_formatter = 'doc'
   end
 
-  config.profile_examples = 10
+  # set to any number to enable profiling
+  config.profile_examples = false
+
   config.order = :random
   Kernel.srand config.seed
 end
