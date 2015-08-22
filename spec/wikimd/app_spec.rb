9131def16fdf22d28eeaeff57bc08feb15c8f817
Manuel Hutter <manuel@hutter.io>
2015-08-22 13:32:39 +0200

    added app specs

--- spec/wikimd/app_spec.rb
+++ spec/wikimd/app_spec.rb
@@ -20,6 +20,28 @@ RSpec.describe WikiMD::App do
     end
   end
 
+  describe 'GET /a_file.md' do
+    before(:each) do
+      @repo = double('repo')
+      allow(WikiMD::Repository).to receive(:new) { @repo }
+    end
+
+    it 'returns the content of the given file' do
+      expect(@repo).to receive(:read).with('a_file.md').and_return('# A File!')
+      get '/a_file.md'
+      expect(last_response.status).to eq 200
+      expect(last_response.body).to include('<h1>A File!</h1>')
+    end
+
+    it 'returns 404 if the file does not exist' do
+      expect(@repo).to receive(:read)
+        .and_raise(WikiMD::Repository::FileNotFound)
+
+      get '/something.md'
+      expect(last_response.status).to eq 404
+    end
+  end
+
   describe 'GET /syntax.css' do
     it 'renders Syntax highlighting CSS' do
       get '/syntax.css'
